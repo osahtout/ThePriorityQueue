181bbcd9b000c59e335d6517410f643f9c72546a
golbr <43707415+golbr@users.noreply.github.com>
2019-03-13 13:50:22 -0400

    latest

--- aspectj_priority_queue/src/main/java/com/PriorityQueue/Grecia/BinaryHeapPQ.java
+++ aspectj_priority_queue/src/main/java/com/PriorityQueue/Grecia/BinaryHeapPQ.java
@@ -1,16 +1,18 @@
-package com.PriorityQueue.Grecia;
-//import com.PriorityQueue.Grecia.Contracts;
+/*******************************************************************************
+ * SOEN 331
+ * Assignment #3
+ * Group Members: Grecia Olano O'Brien ID: 25372232, Moataz Fawzy ID: 27673817,
+ * Omnia Gomaa ID: 40017116, Omar Sahtout ID: 40018126
+ ******************************************************************************/
 
-import org.aspectj.lang.annotation.Aspect;
+import java.util.*;
 
-import java.util.Arrays;
+import be.ac.ua.ansymo.adbc.annotations.ensures;
+import be.ac.ua.ansymo.adbc.annotations.requires;
 
 /* The BinaryHeapPQ class is a Min-Heap implementation.*/
-@Aspect
 public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> implements PriorityQueue<ElementType, KeyType>
-{
-
-
+{	
 	/* Attributes */
 	private int numberOfElements;
 	private final int capacity;
@@ -18,15 +20,17 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 	
 	/* Constructs a new Binary Heap. */
 	@SuppressWarnings("unchecked")
+	@requires ({"capacity > 0"})
 	public BinaryHeapPQ(int capacity)
 	{
+		/* Sets the max. capacity. */
 		this.capacity = capacity;
-		/* Sets the numberOfElements of the elements array to max. capacity */
+		/* Sets the numberOfElements of the elements array to max. capacity. */
 		elements = new Pair[capacity];
 		/* Sets the numberOfElements of the heap to 0 */
 		numberOfElements = 0;
-	}	
-			
+	}		
+	
 	/* Returns the position of the left child of a node. */
 	private int leftChild(int i) 
 	{
@@ -46,7 +50,7 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 	}
 	
 	/* Returns the numberOfElements of the heap. */
-	public int getNumberOfElements()
+	public int getNumberOfElements() 
 	{
 		return numberOfElements;
 	}
@@ -63,12 +67,11 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 		return numberOfElements == 0;
 	}
 	
-	/* Returns true if the numberOfElements of the heap is equal to maxSize, otherwise it returns false. */
+	/* Returns true if the size of the heap is equal to maxSize, otherwise it returns false. */
 	public boolean isFull() 
 	{
 		return numberOfElements == capacity;
 	}
-	
 	/* The swap() method swaps the value of two indexes. */
 	public void swap(int i, int j)
 	{
@@ -95,13 +98,13 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 		/* Compares the key of the left child with the key of the parent, 
 		if the key of the left child comes before the key of the parent,
 		then the key of the left child becomes the min. */ 
-		if (left <= numberOfElements -1 && elements[left].getKey().compareTo(elements[i].getKey()) < 0)
+		if (left <= numberOfElements - 1 && elements[left].getKey().compareTo(elements[i].getKey()) < 0)
 			min = left;
 		
 		/* Compares the key of the right child with the key of the parent,
 		if the key of the right child comes before the key of the parent,
 		then the key of the right child becomes the min. */
-		if (right <= numberOfElements -1 && elements[right].getKey().compareTo(elements[min].getKey()) < 0)
+		if (right <= numberOfElements - 1 && elements[right].getKey().compareTo(elements[min].getKey()) < 0)
 			min = right;
 		
 		/* If the min does not equal the original parent, then the swap() method is invoked,
@@ -128,6 +131,17 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 	
 	/* Implementing methods from the Priority Queue Interface */
 	/* The insert(ElementType el, KeyType key) method inserts the pair (el, key). */
+	@requires 
+	({ 
+		"el != null", 
+		"key > 0", 
+		"$this.isFull() == false"
+	})
+	@ensures 
+	({ 
+		"$this.contains(el)",
+		"$this.getNumberOfElements() == $old($this.getNumberOfElements()) + 1"
+	})
 	public void insert(ElementType el, KeyType key) 
 	{
 		/* Stores the new pair at the bottom of the heap. */ 
@@ -136,23 +150,32 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 		/* Calls the heapifyUp() method. */
 		heapifyUp(numberOfElements);
 		
-		/* Increases the heap numberOfElements */
+		/* Increases the heap size */
 		numberOfElements++;
 	}
-
+	
 	/* The remove() method removes and returns the element with the smallest key. */
+	@requires 
+	({
+		"$this.isEmpty() == false"
+	})
+	@ensures 
+	({
+		"$result != null",
+		"$this.getNumberOfElements() == $old($this.getNumberOfElements()) - 1"
+	})
 	public ElementType remove()
 	{
 		/* Calls the swap() method to swap the top of the heap with the element at the bottom of the heap. */ 
-		swap(0, numberOfElements -1);
+		swap(0, numberOfElements - 1);
 		
 		/* Stores the content of the element at the bottom of the heap (which has already been swapped). */
-		Pair<ElementType, KeyType> temp = elements[numberOfElements -1];
+		Pair<ElementType, KeyType> temp = elements[numberOfElements-1];
 		
 		/* Removes the element from the array. */
-		elements[numberOfElements -1] = null;
+		elements[numberOfElements - 1] = null;
 		
-		/* Decreases the heap numberOfElements. */
+		/* Decreases the heap size. */
 		numberOfElements--;
 		
 		/* Calls the heapifyDown() method to maintain min-heap property. */
@@ -168,31 +191,38 @@ public class BinaryHeapPQ<ElementType, KeyType extends Comparable<KeyType>> impl
 		/* Returns the element at the top of the heap. */
 		return elements[0].getElement();
 	}
-
+		
+	/* The contains() method return true if an element is included in the elements array. */
+	@requires 
+	({
+		"el != null"
+	})
+	public boolean contains(ElementType el)
+	{
+		int count = numberOfElements;
+		boolean result = false;
+		
+		for(int i = 0 ; i < count; i++)
+		{
+			if(el == elements[i].getElement())
+			{
+				result = true;
+				break;
+			}
+		}
+		
+		return result;
+	}
+	
+	/* The asString() method returns a String representation of the elements array. */
 	public void asString()
 	{
 		for(int i = 0; i < elements.length; i++)
 			System.out.println(elements[i]);
 	}
 	
-	/* The max() method returns but does not remove the element with the largest key. */
-	public ElementType max()
-	{
-		/* Returns the element at the bottom of the heap. */
-		if (elements[numberOfElements -1] != null)
-			return elements[numberOfElements -1].getElement();
-		else
-			return elements[numberOfElements -1].getElement();
-	}
-	
-	/* The getElements() returns the elements of array. */
-	public Pair<ElementType, KeyType>[] getElements()
-	{
-		return elements;
-	}
-	
-	/* The toString() method returns a String representation of the elements array. */
 	public String toString() {
         return Arrays.toString(elements);
-    }	
+	}
+	
 }
--- aspectj_priority_queue/src/main/java/com/PriorityQueue/Grecia/PriorityQueueTest.java
+++ aspectj_priority_queue/src/main/java/com/PriorityQueue/Grecia/PriorityQueueTest.java
@@ -1,98 +1,109 @@
-package com.PriorityQueue.Grecia;
+/*******************************************************************************
+ * SOEN 331
+ * Assignment #3
+ * Group Members: Grecia Olano O'Brien ID: 25372232, Moataz Fawzy ID: 27673817,
+ * Omnia Gomaa ID: 40017116, Omar Sahtout ID: 40018126
+ ******************************************************************************/
 
-public class PriorityQueueTest {
-
+public class PriorityQueueTest 
+{
 	public static void main(String[] args) 
 	{
-		System.out.println("-----------------------------");
-
-		BinaryHeapPQ<String, Integer> pqArray = new BinaryHeapPQ<>(10);
-		System.out.println(pqArray.getClass());
-
-
-		pqArray.insert("first", 1);
-		pqArray.insert("last", 10);
-		pqArray.insert("middle", 7);
-		pqArray.insert("middle", 3);
-		pqArray.insert("midl", 4);
-		pqArray.insert("mid", 5);
-
-		pqArray.asString();
-
-		String pop = pqArray.remove();
-		System.out.println(pop);
-		System.out.println("***");
-
-		pqArray.asString();
-		System.out.println("***");
-		System.out.println("#####");
-		System.out.println(pqArray.min());
-		System.out.println("#####");
-
-
-		System.out.println("-----------------------------");
-
-
-
-
-
-	//	testPriorityQueueWithKeyIntegers();
-	    System.out.println();
-	}
-	    
-	public static void testPriorityQueueWithKeyIntegers() 
-	{
-		PriorityQueue<String, Integer> myPQ = new BinaryHeapPQ<String, Integer>(20);
-	    
-		/* Testing the insert(el, key) */
-		myPQ.insert("Element 20", 20);
-		myPQ.insert("Element 18", 18);
-		myPQ.insert("Element 44", 44);
-		myPQ.insert("Element 3", 3);
-		myPQ.insert("Element 13", 13);
-		myPQ.insert("Element 9", 9);
+		// Failed contracts are commented out. //
 		
-		System.out.println(myPQ);
-	    
-		/* Output: 
-		[Element = Element 3 , Key = 3, Element = Element 13 , Key = 13, Element = Element 9 , Key = 9, 
-		Element = Element 20 , Key = 20, Element = Element 18 , Key = 18, Element = Element 44 , Key = 44, 
-		null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-		*/
-			
-		/* Testing the remove() method. */
-		System.out.println(myPQ.remove());
-		/* Output: Element 3 */
+				// Test #1: Creating a Heap with a capacity of 10.
+				BinaryHeapPQ<String, Integer> myHeap = new BinaryHeapPQ<>(10);
+				// Output: 10
+				System.out.println(myHeap.getCapacity());
+				
+				// Test #2: Creating a Heap with a capacity of 0.
+				// BinaryHeapPQ<String, Integer> myHeap = new BinaryHeapPQ<>(0);
+				// Output: 
+				// Precondition broken!
+				// Contract: capacity > 0
+				// Where: public BinaryHeapPQ(int)
+						
+				System.out.println();
+				
+				// Test #3: Inserting elements into the Priority Queue, using a pair of type String, Integer. 
+				BinaryHeapPQ<String, Integer> myPQ1 = new BinaryHeapPQ<>(10);
+				myPQ1.insert("first", 2);
+				myPQ1.insert("last", 10);
+				myPQ1.insert("middle", 7);
+				myPQ1.insert("middle", 3);
+				myPQ1.insert("middle", 4);
+				myPQ1.insert("middle", 5);
+					
+				System.out.println(myPQ1.min());	
+				System.out.println(myPQ1.isEmpty());	
+				System.out.println(myPQ1.isFull());
+				
+				System.out.println(myPQ1.contains("last"));
+				System.out.println(myPQ1.contains("tomato"));
+				
+				// Test #4: Inserting elements into the Priority Queue, leaving the first member of the pair (of String type), empty.
+				// BinaryHeapPQ<String, Integer> myPQ2 = new BinaryHeapPQ<>(2);
+				// myPQ2.insert(null, 1);
+				// Output:
+				// Precondition broken!
+				// Contract: el != null
+				// Where: public void BinaryHeapPQ.insert(java.lang.Object, java.lang.Comparable)
+				// Blame: PriorityQueueTest.main
 
-		/* While the Priority Queue is not empty, remove and return the element with the smallest key. */
-	    while (!myPQ.isEmpty()) 
-	    {
-	    	System.out.println(myPQ.remove());               
-	        System.out.println(myPQ);
-	    }
-	    
-	    /* Output:		
-	    Element 9
-	    [Element = Element 13 , Key = 13, Element = Element 18 , Key = 18, Element = Element 44 , Key = 44, Element = Element 20 , Key = 20, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-	    Element 13
-	    [Element = Element 18 , Key = 18, Element = Element 20 , Key = 20, Element = Element 44 , Key = 44, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-	    Element 18
-	    [Element = Element 20 , Key = 20, Element = Element 44 , Key = 44, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-	    Element 20
-	    [Element = Element 44 , Key = 44, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-	    Element 44
-	    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
-	    */
-	    
-	    myPQ.insert("Toyota", 2);
-	    myPQ.insert("Mercedes", 5);
-	    myPQ.insert("Audi", 1);
-	    
-	    /* Testing the min() method. */
-	    System.out.println(myPQ.min());
-	    
-	    /* Output:
-	    Audi 
-	    */
+				// Test #5: Inserting elements into the Priority Queue, entering a key of value.
+				// BinaryHeapPQ<String, Integer> myPQ3 = new BinaryHeapPQ<>(2);
+				// myPQ3.insert("first", 0);
+				// Output:
+				// Precondition broken!
+				// Contract: key > 0
+				// Where: public void BinaryHeapPQ.insert(java.lang.Object, java.lang.Comparable)
+				// Blame: PriorityQueueTest.main
+								
+				// Test #6:  
+				// System.out.println(myPQ1.contains(null));
+				// Precondition broken!
+				// Contract:	el != null
+				// Where:		public boolean BinaryHeapPQ.contains(java.lang.Object)
+				// Blame:		PriorityQueueTest.main		
+
+				//pqArray.asString();
+				
+				/*
+				PriorityQueue<String, Integer> myPQ = new BinaryHeapPQ<String, Integer>(10);
+			    
+				/* Testing the insert(el, key) 
+				myPQ.insert("Element 20", 20);
+				myPQ.insert("Element 18", 18);
+				myPQ.insert("Element 44", 44);
+				myPQ.insert("Element 3", 3);
+				myPQ.insert("Element 13", 13);
+				myPQ.insert("Element 9", 9);
+				
+				myPQ.asString();
+				System.out.println();
+					
+				/* Testing the remove() method. 
+				System.out.println(myPQ.remove());
+				System.out.println();
+				/* Output: Element 3 */
+
+				/* While the Priority Queue is not empty, remove and return the element with the smallest key. 
+			    while (!myPQ.isEmpty()) 
+			    {
+			    	System.out.println(myPQ.remove());               
+			    	myPQ.asString();
+			    	System.out.println();  
+			    }
+			        
+			    myPQ.insert("Toyota", 2);
+			    myPQ.insert("Mercedes", 5);
+			    myPQ.insert("Audi", 1);
+			    
+			    /* Testing the min() method. 
+			    System.out.println(myPQ.min());
+			    
+			    /* Output:
+			    Audi 
+			    */
 	}
 }
